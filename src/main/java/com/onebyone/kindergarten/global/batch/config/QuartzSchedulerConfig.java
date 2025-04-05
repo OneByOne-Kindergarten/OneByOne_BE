87a242519b13d4c4b872253de7777d552ffc0a97
wngns1101 <wngns0812@gmail.com>
2025-04-05 22:11:29 +0900

    chore(batch): 샘플 스케쥴러 제거

--- src/main/java/com/onebyone/kindergarten/global/batch/config/QuartzSchedulerConfig.java
+++ src/main/java/com/onebyone/kindergarten/global/batch/config/QuartzSchedulerConfig.java
@@ -1,31 +1,31 @@
-package com.onebyone.kindergarten.global.batch.config;
-
-import com.onebyone.kindergarten.global.batch.job.InstantJob;
-import org.quartz.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class QuartzSchedulerConfig {
-
-    @Bean
-    public JobDetail instantDetail() {
-        return JobBuilder.newJob(
-                InstantJob.class
-        ).storeDurably()
-                .build();
-    }
-
-    @Bean
-    public Trigger instantTrigger() {
-        return TriggerBuilder.newTrigger()
-                .forJob(instantDetail())
-                .startNow()
-                .withSchedule(instantCronScheduler())
-                .build();
-    }
-
-    public CronScheduleBuilder instantCronScheduler() {
-        return CronScheduleBuilder.cronSchedule("0 * * * * ?");
-    }
-}
+//package com.onebyone.kindergarten.global.batch.config;
+//
+//import com.onebyone.kindergarten.global.batch.job.InstantJob;
+//import org.quartz.*;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class QuartzSchedulerConfig {
+//
+//    @Bean
+//    public JobDetail instantDetail() {
+//        return JobBuilder.newJob(
+//                InstantJob.class
+//        ).storeDurably()
+//                .build();
+//    }
+//
+//    @Bean
+//    public Trigger instantTrigger() {
+//        return TriggerBuilder.newTrigger()
+//                .forJob(instantDetail())
+//                .startNow()
+//                .withSchedule(instantCronScheduler())
+//                .build();
+//    }
+//
+//    public CronScheduleBuilder instantCronScheduler() {
+//        return CronScheduleBuilder.cronSchedule("0 * * * * ?");
+//    }
+//}
--- src/main/java/com/onebyone/kindergarten/global/batch/job/InstantJob.java
+++ src/main/java/com/onebyone/kindergarten/global/batch/job/InstantJob.java
@@ -1,17 +1,17 @@
-package com.onebyone.kindergarten.global.batch.job;
-
-import lombok.extern.slf4j.Slf4j;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.springframework.scheduling.quartz.QuartzJobBean;
-
-@Slf4j
-public class InstantJob extends QuartzJobBean {
-
-    @Override
-    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        log.info("=====Instant job started=====");
-        log.info("instantJob");
-        log.info("=====Instant job completed=====");
-    }
-}
+//package com.onebyone.kindergarten.global.batch.job;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.quartz.JobExecutionContext;
+//import org.quartz.JobExecutionException;
+//import org.springframework.scheduling.quartz.QuartzJobBean;
+//
+//@Slf4j
+//public class InstantJob extends QuartzJobBean {
+//
+//    @Override
+//    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+//        log.info("=====Instant job started=====");
+//        log.info("instantJob");
+//        log.info("=====Instant job completed=====");
+//    }
+//}
