c102d8b0615ff0b9a48c619270f0ad41621f27ea
wngns1101 <wngns0812@gmail.com>
2025-05-13 14:20:34 +0900

    feat(CI/CD): docker, github action 적용

--- src/main/java/com/onebyone/kindergarten/global/config/FirebaseConfig.java
+++ src/main/java/com/onebyone/kindergarten/global/config/FirebaseConfig.java
@@ -5,20 +5,28 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import jakarta.annotation.PostConstruct;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
+    @Value("${firebase.service-account}")
+    private String serviceAccountBase64;
+
     @PostConstruct
     public void init(){
         try{
+//            InputStream serviceAccount = new ClassPathResource("OnebyoneFirebaseKey.json").getInputStream();
 
-            /// 파이어베이스 인증키 파일
-            InputStream serviceAccount = new ClassPathResource("OnebyoneFirebaseKey.json").getInputStream();
+            // 파이어베이스 인증키 파일
+            InputStream serviceAccount = new ByteArrayInputStream(getBase64DecodeBytes(serviceAccountBase64));
 
             /// 파이어베이스 초기화
             FirebaseOptions options = FirebaseOptions.builder()
@@ -33,7 +41,11 @@ public class FirebaseConfig {
             throw new RuntimeException("Failed to initialize Firebase", e);
         }
     }
-    
+
+    private static byte[] getBase64DecodeBytes(String input) {
+        return Base64.decodeBase64(input);
+    }
+
     @Bean
     public FirebaseMessaging firebaseMessaging() {
         return FirebaseMessaging.getInstance();
