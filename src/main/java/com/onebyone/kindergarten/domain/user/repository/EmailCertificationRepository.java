25555259b7af8ed0965d3c8e49a9ae6ce882cd66
unknown <ucs06>
2025-08-26 15:36:33 +0900

    feat(conf): grafana url ë추가

--- src/main/java/com/onebyone/kindergarten/domain/user/repository/EmailCertificationRepository.java
+++ src/main/java/com/onebyone/kindergarten/domain/user/repository/EmailCertificationRepository.java
@@ -1,9 +1,10 @@
 package com.onebyone.kindergarten.domain.user.repository;
 
+import org.springframework.data.keyvalue.repository.KeyValueRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.onebyone.kindergarten.domain.user.entity.EmailCertification;
 
-public interface EmailCertificationRepository extends CrudRepository<EmailCertification, String> {
+public interface EmailCertificationRepository extends KeyValueRepository<EmailCertification, String> {
 
 }
