c39438c038fb69c1f1a5284cf1be1fefd3782fe6
wngns0812 <wngns0812@gmail.com>
2025-09-19 16:20:10 +0900

    fix(filter): 토큰 예외 수정

--- src/main/java/com/onebyone/kindergarten/global/jwt/JwtEntryPoint.java
+++ src/main/java/com/onebyone/kindergarten/global/jwt/JwtEntryPoint.java
@@ -1,8 +1,9 @@
 package com.onebyone.kindergarten.global.jwt;
 
-import com.onebyone.kindergarten.global.exception.BusinessException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.onebyone.kindergarten.global.exception.ErrorCodes;
-import lombok.extern.slf4j.Slf4j;
+import com.onebyone.kindergarten.global.exception.ErrorResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -10,12 +11,27 @@ import org.springframework.stereotype.Component;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Slf4j
+import java.io.IOException;
+
 @Component
+@RequiredArgsConstructor
 public class JwtEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper om;
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
-        throw new BusinessException(ErrorCodes.INVALID_TOKEN_EXPIRED);
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        
+        try {
+            ErrorResponse body = ErrorResponse.buildError(ErrorCodes.INVALID_TOKEN_EXPIRED);
+            om.writeValue(response.getWriter(), body);
+        } catch (IOException e) {
+
+        }
     }
 }
