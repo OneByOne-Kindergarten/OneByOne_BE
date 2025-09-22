42a169e91361c591f9f6c4a607f81f096aba67ed
wngns0812 <wngns0812@gmail.com>
2025-09-22 16:53:49 +0900

    fix(api): 예외 처리 수정

--- src/main/java/com/onebyone/kindergarten/global/exception/ErrorCodes.java
+++ src/main/java/com/onebyone/kindergarten/global/exception/ErrorCodes.java
@@ -50,6 +50,8 @@ public enum ErrorCodes {
     FAILED_TOP_POST_CACHE_EXCEPTION("E0044", "인기 게시글 캐시 갱신에 실패했습니다."),
     FAILED_SEND_MAIL_EXCEPTION("E0045", "메일 발송에 실패했습니다"),
     ALREADY_EXIST_EMAIL_CERTIFICATION("E0046", "기존에 발급된 인증번호가 존재합니다."),
+    FAILED_EMAIL_CERTIFICATION_EXCEPTION("E0047", "인증되지 않은 이메일입니다."),
+    FAILED_AUTHORIZATION_EXCEPTION("E0048", "인증되지 않은 요청입니다."),
     INTERNAL_SERVER_ERROR("E9999","알 수 없는 에러 발생");
 
     private final String code;
