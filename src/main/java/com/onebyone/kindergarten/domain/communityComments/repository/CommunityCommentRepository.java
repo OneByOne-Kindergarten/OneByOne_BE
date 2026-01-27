9c9f91e6c6dacfa93e1ac13a2973a4d35f42040d
한승완 <hseungwan8@gmail.com>
2026-01-27 15:14:37 +0900

    feat: 커뮤니티 API 사용자 리뷰 작성 여부 플래그 추가 (2)

--- src/main/java/com/onebyone/kindergarten/domain/communityComments/repository/CommunityCommentRepository.java
+++ src/main/java/com/onebyone/kindergarten/domain/communityComments/repository/CommunityCommentRepository.java
@@ -19,7 +19,7 @@ public interface CommunityCommentRepository extends JpaRepository<CommunityComme
   // 게시글의 원댓글 목록 조회 (parent IS NULL)
   @Query(
       "SELECT new com.onebyone.kindergarten.domain.communityComments.dto.response.CommentResponseDTO("
-          + "c.id, c.content, u.nickname, u.email, u.career, u.role, c.createdAt, c.status, "
+          + "c.id, c.content, u.nickname, u.email, u.career, u.role, u.hasWrittenReview, c.createdAt, c.status, "
           + "null, false) "
           + "FROM community_comment c "
           + "JOIN c.user u "
@@ -31,7 +31,7 @@ public interface CommunityCommentRepository extends JpaRepository<CommunityComme
   // 특정 원댓글에 대한 대댓글 목록 조회
   @Query(
       "SELECT new com.onebyone.kindergarten.domain.communityComments.dto.response.CommentResponseDTO("
-          + "c.id, c.content, u.nickname, u.email, u.career, u.role, c.createdAt, c.status, "
+          + "c.id, c.content, u.nickname, u.email, u.career, u.role, u.hasWrittenReview, c.createdAt, c.status, "
           + "c.parent.id, true) "
           + "FROM community_comment c "
           + "JOIN c.user u "
@@ -42,7 +42,7 @@ public interface CommunityCommentRepository extends JpaRepository<CommunityComme
   // 게시글의 모든 댓글 조회 (대댓글 포함) - 최적화 버전
   @Query(
       "SELECT new com.onebyone.kindergarten.domain.communityComments.dto.response.CommentResponseDTO("
-          + "c.id, c.content, u.nickname, u.email ,u.career, u.role, c.createdAt, c.status, "
+          + "c.id, c.content, u.nickname, u.email ,u.career, u.role, u.hasWrittenReview, c.createdAt, c.status, "
           + "c.parent.id, CASE WHEN c.parent IS NOT NULL THEN true ELSE false END) "
           + "FROM community_comment c "
           + "JOIN c.user u "
